602b9abd79afbce49e18e88b024d866f310c356d
DPL <dplandigi@outlook.com>
2021-07-30 15:49:45 +0530

    Create Welcome.sh

---
+++ Welcome.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Welcome to the world of DplOps"
+echo "What is your name:"
+read name
+echo "Why did you came here:"
+read came
+echo "Thank you $name, like the reason $came."
+exit
